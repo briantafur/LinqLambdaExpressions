fb905e7055c92c63a70c0cf0a2aaae5635f4541c
Brian David Tafur Londoño <Brian.tafur@yuxiglobal.com>
2017-06-21 17:31:04 -0500

    First commit

---
+++ LinqLambdaPractice/Platform.Modeler/DAO/EstudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Platform.Modeler.Entity;
+
+namespace Platform.Modeler.DAO
+{
+
+    public class EstudentController
+    {
+
+        EntityStudentDataContext db;
+
+        public EstudentController()
+        {
+            db = new EntityStudentDataContext();
+        }
+
+        public bool guardar(String codigo, String nombre, String apellido, int edad, String carrera, String semestre)
+        {
+            //db.guardarEstudiante(codigo, nombre, apellido, edad, carrera, Convertsemestre);
+            return true;
+        }
+
+
+    }
+}
